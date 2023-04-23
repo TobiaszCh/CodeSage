14fc1a6a0c1ebe6557d9627328d1181c6a61b67d
Tobiasz <tobijas.ch@gmail.com>
2023-04-23 18:58:31 +0200

    change MySql for PostgresSQL and small corrects in project

---
+++ src/main/java/com/educator/dto/CourseDto.java
@@ -0,0 +1,12 @@
+package com.educator.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CourseDto {
+
+    private int id;
+    private String displayName;
+}

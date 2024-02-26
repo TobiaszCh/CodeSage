434def0a74c91f3d1594eca2c8d63f301a918a59
Tobiasz <tobijas.ch@gmail.com>
2024-02-26 05:40:02 +0100

    work with question and answer

--- src/main/java/com/educator/question/Question.java
+++ src/main/java/com/educator/question/Question.java
@@ -1,10 +1,13 @@
 package com.educator.question;
+import com.educator.answer.Answer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Getter
 @Entity
@@ -15,9 +18,12 @@ public class Question {
     @GeneratedValue
     private Long id;
 
-    private String displayNane;
+    private String displayName;
+    @OneToMany(mappedBy = "question")
+    private List<Answer> answers;
 
-
-
-
+    public Question(Long id, String displayName) {
+        this.id = id;
+        this.displayName = displayName;
+    }
 }
--- src/main/java/com/educator/user_subject/UserSubject.java
+++ src/main/java/com/educator/user_subject/UserSubject.java
@@ -1,4 +1,11 @@
 package com.educator.user_subject;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
 public class UserSubject {
+    @Id
+    @GeneratedValue
+    private Long id;
 }

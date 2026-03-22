d3204f44a3e8ae5f5d19e0cbfd57f3ff9f73ce44
Tobiasz <tobijas.ch@gmail.com>
2026-03-22 01:04:16 +0100

    Feature: - Added trim to method

--- src/main/java/com/educator/core/course/CourseDto.java
+++ src/main/java/com/educator/core/course/CourseDto.java
@@ -2,11 +2,13 @@ package com.educator.core.course;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class CourseDto {
 
--- src/main/java/com/educator/core/subject/dto/SubjectDto.java
+++ src/main/java/com/educator/core/subject/dto/SubjectDto.java
@@ -1,12 +1,14 @@
 package com.educator.core.subject.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class SubjectDto {
 
